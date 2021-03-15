6acb8061a85373fe87f3f21856aeeb5ed8359357
Swapnanil Sharma <swapnanilsharma@gmail.com>
2021-03-15 21:53:15 +0530

    Add files via upload

---
+++ UserManagement.py
@@ -0,0 +1,180 @@
+import json
+import requests
+import logging
+import time
+import os
+import ast
+
+
+class userManagement():
+
+    def __init__(self, envUrl, bearerToken):
+        self.envUrl = envUrl
+        self.bearerToken = bearerToken
+        self.createUserApi = "api/user/v1/create"
+        self.addUsersToOrgApi = "api/org/v1/member/add"
+        self.searchUserApi = "api/user/v1/search"
+        self.editUserApi = "api/user/v1/update"
+        self.assignRoleApi = "api/user/v1/role/assign"
+        self.createUserTokenCredential = {
+                                          "url": "auth/realms/sunbird/protocol/openid-connect/token",
+                                          "client_id": "admin-cli",
+                                          "username": "admin",
+                                          "password": "password",
+                                          "grant_type": "password"
+                                         }
+
+    def searchUser(self, searchString=None):
+        url = os.path.join(self.envUrl, self.searchUserApi).replace("\\","/")
+        payload = json.dumps({"request": {"query": searchString, "filters": {}, "limit": 10000}})
+        headers = {
+                    'authorization': self.bearerToken,
+                    'content-type': 'application/json',
+                    'x-authenticated-user-token': self.generateUserToken()
+                  }
+
+        response = requests.request("POST", url, data=payload, headers=headers)
+        logging.info(f">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> SEARCH USER <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+        logging.info(f"PAYLOAD: {payload}")
+        logging.info(f"HEADERS: {headers}")
+        logging.info(f"URL: {url}")
+        logging.info(f"RESPONSE_CODE: {response.status_code}")
+        logging.info(f"RESPONSE: {response.text}")
+        return json.loads(response.text).get("result").get("response")
+
+    def editUser(self, userId, firstName, dob):
+        url = os.path.join(self.envUrl, self.editUserApi).replace("\\","/")
+        headers = {
+                    'authorization': self.bearerToken,
+                    'content-type': 'application/json',
+                    'x-authenticated-user-token': self.generateUserToken()
+                  }
+        payload = ""
+        response = requests.request("POST", url, data=payload, headers=headers)
+        logging.info(f">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> EDIT USER <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+        logging.info(f"PAYLOAD: {payload}")
+        logging.info(f"HEADERS: {headers}")
+        logging.info(f"URL: {url}")
+        logging.info(f"RESPONSE_CODE: {response.status_code}")
+        logging.info(f"RESPONSE: {response.text}")
+        return json.loads(response.text)
+
+    def generateUserToken(self):
+        url = os.path.join(self.envUrl, self.createUserTokenCredential.get('url')).replace("\\","/")
+
+        payload = 'client_id=' + self.createUserTokenCredential.get('client_id') + \
+                  '&username=' + self.createUserTokenCredential.get('username') + \
+                  '&password=' + self.createUserTokenCredential.get('password') + \
+                  '&grant_type=' + self.createUserTokenCredential.get('grant_type')
+        headers = {
+                   'Content-Type': 'application/x-www-form-urlencoded'
+                  }
+        response = requests.request("POST", url, headers=headers, data = payload)
+        logging.info(f">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> GENERATE JWT USER TOKEN <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+        logging.info(f"PAYLOAD: {payload}")
+        logging.info(f"HEADERS: {headers}")
+        logging.info(f"URL: {url}")
+        logging.info(f"RESPONSE_CODE: {response.status_code}")
+        logging.info(f"RESPONSE: {response.text}")
+        return json.loads(response.text).get('access_token')
+
+    def createSingleUser(self, firstName, lastName, email, channel, phoneNumber=None, password=None):
+        url = os.path.join(self.envUrl, self.createUserApi).replace("\\","/")
+        headers = {
+                   'Accept': 'application/json',
+                   'Content-Type': 'application/json',
+                   'Authorization': self.bearerToken
+                  }
+
+        payload = dict()
+        d1 = dict()
+        d1['firstName'] = firstName
+        d1['lastName'] = lastName
+        d1['email'] = email
+        if phoneNumber is None:
+            d1['phone'] = str(random.randint(8700000000, 9999999999))
+        else:
+            d1['phone'] = str(phoneNumber)
+        if password is None:
+            pass
+        else:
+            d1['password'] = password
+        d1['userName'] = "_".join(firstName.split()).lower() + "_" + "_".join(lastName.split()).lower()
+        d1['emailVerified'] = True
+        d1['phoneVerified'] = True
+        d1['channel'] = channel
+
+        payload['request'] = d1
+        payload =  json.dumps(payload)
+
+        response = requests.request("POST", url, headers=headers, data = payload)
+        logging.info(f">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> CREATE USER <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+        logging.info(f"PAYLOAD: {payload}")
+        logging.info(f"HEADERS: {headers}")
+        logging.info(f"URL: {url}")
+        logging.info(f"RESPONSE_CODE: {response.status_code}")
+        logging.info(f"RESPONSE: {response.text}")
+        try:
+            self.userId = json.loads(response.text)["result"]["userId"] #.get('result').get('userId')
+        except:
+            self.userId = None
+            return "Failed to create new user !!"
+        return f"Userid {self.userId} created successfully"
+
+    def addUsertoOrg(self, orgId, roles):
+        userToken = self.generateUserToken()
+        url = os.path.join(self.envUrl, self.addUsersToOrgApi).replace("\\","/")
+
+        payload = dict()
+        d1 = dict()
+        if self.userId is None:
+            return "User is not created"
+        d1['userId'] = self.userId
+        d1['organisationId'] = orgId
+        d1['roles'] = ast.literal_eval(roles)
+        payload['request'] = d1
+        payload =  json.dumps(payload)
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': self.bearerToken,
+            'X-Authenticated-User-Token': userToken
+            }
+        response = requests.request("POST", url, headers=headers, data = payload)
+        logging.info(f">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> LINK USER TO ORG <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+        logging.info(f"PAYLOAD: {payload}")
+        logging.info(f"HEADERS: {headers}")
+        logging.info(f"URL: {url}")
+        logging.info(f"RESPONSE_CODE: {response.status_code}")
+        logging.info(f"RESPONSE: {response.text}")
+        link_status = json.loads(response.text).get('responseCode')
+        return link_status
+
+    def assignRole(self, orgId, roles):
+        userToken = self.generateUserToken()
+        url = os.path.join(self.envUrl, self.assignRoleApi).replace("\\","/")
+
+        payload = dict()
+        d1 = dict()
+        if self.userId is None:
+            return "User is not created"
+        d1['userId'] = self.userId
+        d1['organisationId'] = orgId
+        d1['roles'] = ast.literal_eval(roles)
+        payload['request'] = d1
+        payload =  json.dumps(payload)
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': self.bearerToken,
+            'X-Authenticated-User-Token': userToken
+            }
+        response = requests.request("POST", url, headers=headers, data = payload)
+        logging.info(f">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> LINK USER TO ORG <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+        logging.info(f"PAYLOAD: {payload}")
+        logging.info(f"HEADERS: {headers}")
+        logging.info(f"URL: {url}")
+        logging.info(f"RESPONSE_CODE: {response.status_code}")
+        logging.info(f"RESPONSE: {response.text}")
+        link_status = json.loads(response.text).get('responseCode')
+        return link_status
---
+++ streamlitServ.py
@@ -0,0 +1,185 @@
+import schedule
+import time
+import datetime
+import pandas as pd
+import sqlite3
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import smtplib
+import os
+import requests
+import json
+from UserManagement import userManagement
+from statistics import mean, StatisticsError
+from tqdm import tqdm
+import streamlit as st
+from stqdm import stqdm
+import base64
+
+if not os.path.exists("SchedularFiles"):
+    os.mkdir("SchedularFiles")
+
+
+# MYJBR DETAILS
+envUrl="https://myjbr.flexresourcemanagement.com"
+bearerToken="Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI3NWM2MjU2OGI0NDQ0MTRmODdmZTljM2M3MWE0NTdlMSJ9.0bKHF_wWZiMoqhkcB6TZsmpZGNLhCg3xr-N9OPDTi9I"
+
+
+def get_table_download_link(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    href = f'<a href="data:file/csv;base64,{b64}" download="download.csv">Download data as csv</a>'
+    return href
+
+def getBatchDetail(envUrl, bearerToken, userToken):
+    url = os.path.join(envUrl, "api/course/v1/batch/list")
+    payload = {"request":{"filters":{"status": 1}}}
+    headers = {
+                "content-type": "application/json",
+                "authorization": bearerToken,
+                "x-authenticated-user-token": userToken,
+                "accept": "application/json"
+    }
+    response = requests.request("POST", url, json=payload, headers=headers)
+    return [{"batchId":i.get("identifier"), "courseId": i.get("courseId"), "batchName": i.get("name")} for i in json.loads(response.text).get("result").get("response").get("content")]
+
+def getBatchEnrolledUsers(envUrl, bearerToken, userToken, batchId):
+    url = os.path.join(envUrl, "api/course/v1/batch/participants/list")
+    headers = {
+        'authorization': bearerToken,
+        'content-type': "application/json",
+        'accept': "application/json",
+        'x-authenticated-user-token': userToken
+    }
+    payload = {"request":{"batch":{"batchId":batchId}}}
+    response = requests.request("POST", url, json=payload, headers=headers)
+    users = json.loads(response.text).get("result").get("batch").get("participants")
+    return users
+    
+    
+
+def getCourseStatus(envUrl, bearerToken, userToken, userId, courseId, batchId):
+    url = os.path.join(envUrl, "content/course/v1/content/state/read")
+    payload = {
+        "request": {
+            "userId": userId,
+            "batchId": batchId,
+            "courseId": courseId
+        }
+    }
+    headers = {
+        'authorization': bearerToken,
+        'content-type': "application/json",
+        'accept': "application/json",
+        'x-authenticated-user-token': userToken
+    }
+    response = requests.request("POST", url, json=payload, headers=headers)
+    contents = json.loads(response.text).get("result").get("contentList")
+    try:
+        mean_status = mean([content.get("status") for content in contents])
+        if mean_status == 2.0:
+            return "Completed"
+        elif 0.0 < mean_status < 2.0:
+            return "In Progress"
+        else:
+            return "Not Started"
+    except:
+        return -1
+
+
+def sendEmail(receiver, cc, subject, message, filename,
+              bcc="egovsup2ort@gmail.com", sender="egovsup2ort@gmail.com"):
+    data = MIMEMultipart()
+    data['From'] = sender
+    data['To'] = receiver
+    data['Cc'] = cc
+    data['Bcc'] = bcc
+    data['Subject'] = subject
+    data.attach(MIMEText(message, 'html'))
+    try:
+        attachment = open("{}".format(filename), "rb")
+        p = MIMEBase('application', 'octet-stream')
+        p.set_payload((attachment).read())
+        encoders.encode_base64(p)
+        p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+        data.attach(p)
+    except FileNotFoundError:
+        pass
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(sender, "pwc@1234")
+    text = data.as_string()
+    s.sendmail(receiver, sender, text)
+    s.quit()
+
+
+if __name__ == "__main__":
+
+    st.header("GENERATE REPORT")
+
+    uploaded_file = st.file_uploader(label="Supporting file(s)", accept_multiple_files=False,
+                                     type=["xls", "xlsx"])
+
+    if st.button("SUBMIT"):
+        if uploaded_file is not None:
+            df_shivam = pd.read_excel(uploaded_file)
+
+        userList = [i for i in df_shivam["user_id"].to_list() if not isinstance(i, int)]
+        lenUserList = len(userList)
+        user = userManagement(envUrl, bearerToken)
+        userToken = user.generateUserToken()
+        results = user.searchUser().get("content")
+        
+        batches = getBatchDetail(envUrl, bearerToken, userToken)
+        b0_enroll = getBatchEnrolledUsers(envUrl, bearerToken, userToken, batchId=batches[0].get("batchId"))
+        b1_enroll = getBatchEnrolledUsers(envUrl, bearerToken, userToken, batchId=batches[1].get("batchId"))
+        
+        out_table = []
+        for userId in stqdm(userList, desc="Generating course progress report.........", mininterval=1):
+            c1_status = getCourseStatus(envUrl, bearerToken, userToken, userId,
+                                        courseId=batches[0].get("courseId"),
+                                        batchId=batches[0].get("batchId"))
+        
+            c2_status = getCourseStatus(envUrl, bearerToken, userToken, userId,
+                                        courseId=batches[1].get("courseId"),
+                                        batchId=batches[1].get("batchId"))
+        
+            if c1_status == -1:
+                if userId in b0_enroll:
+                    c1_status = "Course Assigned but not started"
+                else:
+            	    c1_status = "Course Not Assigned"
+        
+            if c2_status == -1:
+                if userId in b1_enroll:
+            	    c2_status = "Course Assigned but not started"
+                else:
+            	    c2_status = "Course Not Assigned"
+        
+        
+        
+        
+            out_table.append({"user_id": userId,
+                              batches[0].get("batchName"): c1_status,
+                              batches[1].get("batchName"): c2_status
+                              })
+
+        df_swap = pd.DataFrame(out_table)
+        df = pd.merge(df_shivam, df_swap, on='user_id', how='outer')
+        st.write(df)
+        df.to_csv("MYJBR_COURSE_STATUS.csv", index=False)
+        st.markdown(get_table_download_link(df), unsafe_allow_html=True)
+
+        sendEmail(receiver="swapnanilsharma+pwc@gmail.com",
+                  subject="Course Status report for MYJBR dated {}".format(str(datetime.date.today())),
+                  message="""<p>Hi Team,</p>
+                             <p><br></p>
+                             <p>Please find attached the MYJBR Course Status Report on dated {}.</p>
+                             <p><br></p>
+                             <p>Best Regards,</p>
+                             <p>eGov Support Team</p>""".format(str(datetime.date.today())),
+                  cc="ssofficial90@gmail.com",
+                  filename="MYJBR_COURSE_STATUS.csv")
+    
