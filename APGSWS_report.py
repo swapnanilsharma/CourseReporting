81ae4ff254254cb91a383e194df51c4527c63fc6
swapnanilsharma <swapnanilsharma@gmail.com>
2021-08-24 19:19:25 +0530

    report for all the courses

--- APGSWS_report.py
+++ APGSWS_report.py
@@ -11,9 +11,21 @@ userSearchAPI = "api/user/v1/search"
 batchParticipantsAPI = "api/course/v1/batch/participants/list"
 userTokenAPI = "auth/realms/sunbird/protocol/openid-connect/token"
 courseStatusAPI = "api/course/v1/content/state/read"
+courseSearchAPI = "api/course/v1/search"
+courseBatchListAPI = "api/course/v1/batch/list"
 
-courseId = "do_1133310849852375041158"
-batchId = "0133314135182131206"
+
+#courseId = "do_1133310849852375041158"
+#batchId = "0133314135182131206"
+#courseName = 'GSWS course'
+
+'''
+[{'0133314135182131206': {'do_1133310849852375041158': 'GSWS course'}},
+ {'0133405742222786568': {'do_1133379319085383681314': 'Induction Training to Welfare & Education Assistants '}},
+ {'0133342681988874247': {'do_1133337164316508161240': 'Induction Training to Engineering Assistants'}},
+ {'0133464363032084489': {'do_1133464303906488321366': 'Sample Course'}}]
+
+'''
 
 bearerToken = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJqQXk2ME9rWlNxYVNMZmdIWkk5RGZ4WFdqV3pCTllaZiJ9.Z2Xlyfsem7pAUSQb21Pn92fRYcaKUriBSGBd_FmR6EY"
 
@@ -27,6 +39,25 @@ def userTokenGenerate(bearerToken):
     }
     response = requests.request("POST", url, data=payload, headers=headers)
     return json.loads(response.text).get("access_token")
+
+def getAllBatches():
+    url = os.path.join(host, courseSearchAPI)
+    payload = {"request": {"query": "", "filters": {}}}
+    headers = {
+        'authorization': bearerToken,
+        'x-authenticated-user-token': userTokenGenerate(bearerToken),
+        'content-type': "application/json",
+        'accept': "application/json"
+    }
+    response = requests.request("POST", url, json=payload, headers=headers)
+    courses = json.loads(response.text).get("result").get("course")
+    courses = dict([(course.get("identifier"), course.get("name")) for course in courses])
+
+    url = os.path.join(host, courseBatchListAPI)
+    payload = {"request":{"filters":{"courseId": list(courses.keys()),"status":"1"}}}
+    response = requests.request("POST", url, json=payload, headers=headers)
+    contents = json.loads(response.text).get("result").get("response").get("content")
+    return [{content.get("identifier"): {content.get("courseId"): courses.get(content.get("courseId"))}} for content in contents]
 
 def courseParticipants(bearerToken, batchId):
     url = os.path.join(host, batchParticipantsAPI)
@@ -90,12 +121,12 @@ def getCourseStatus(userId, courseId, batchId):
         else:
             return "Not Started"
     except:
-        return -1
+        return "Not Enrolled"
 
 
-def dataframeToCsv(df, courseId=courseId, batchId=batchId):
+def dataframeToCsv(df, courseId, batchId):
     df.drop(['Age'], axis=1, inplace=True)
-    df["GSWS_Course"] = ""
+    df[courseName] = ""
     for row in tqdm(df.iterrows(), desc="Generating course progress report.........", mininterval=1):
         try:
             status = getCourseStatus(userId=row[1]['userId'], courseId=courseId, batchId=batchId)
@@ -122,12 +153,19 @@ def dataframeToCsv(df, courseId=courseId, batchId=batchId):
                             except:
                                 break
                     
-        row[1]['GSWS_Course'] = status
-    df.to_csv("APGSWS_Report.csv")
+        row[1][courseName] = status
+    df.to_csv(f"APGSWS_Report_{batchId}.csv")
 
 
-userList = getUserDetails(courseParticipants(bearerToken, batchId))
-dataframeToCsv(getUserCSV(userList))
+if __name__ == "__main__":
+    allBathes = getAllBatches()
+    for i in allBathes:
+        batchId = list(i.keys())[0]
+        courseId = list(list(i.values())[0].keys())[0]
+        courseName = list(list(i.values())[0].values())[0]
+        userList = getUserDetails(courseParticipants(bearerToken, batchId))
+        dataframeToCsv(getUserCSV(userList), courseId=courseId, batchId=batchId)
+        break
 
 
 
